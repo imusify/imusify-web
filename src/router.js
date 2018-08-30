28ef5df2c6220bee08e1f7e6a4b870f6bb37354d
Anthony Ettinger <anthony@chovy.com>
2018-08-30 09:07:33 +0000

    Feature/album results

--- src/router.js
+++ src/router.js
@@ -23,7 +23,7 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     }, {
-      path: '/login',
+      path: '/signin',
       name: 'login',
       component: Login,
     }, {
