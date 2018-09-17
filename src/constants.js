8e0d28d702f11d7c17c56068d61eaf8e6161ed00
Anthony Ettinger <anthony@chovy.com>
2018-09-17 12:13:47 -0700

    add localization files for de and en
    - add support for i18n

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+import en from './l10n/en';
+import de from './l10n/de';
+
+// i18n
+const messages = {
+  en,
+  de,
+};
+
+const languages = [{
+  label: 'English',
+  value: 'en',
+}, {
+  label: 'Deutsche',
+  value: 'de',
+}];
+
+export {
+  languages,
+  messages,
+};
