ba78c22209ceefadcd0a8d06ddaf701e311f6f86
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2018-10-26 15:55:35 +0100

    Add spacing (#116)
    * Increase the track timeline lenght at the bottom, move the volume level bar to the right
    
    * Fix: Increase the space between subheading and input fields

--- src/components/SignupNext.vue
+++ src/components/SignupNext.vue
@@ -1,17 +1,17 @@
 <template>
   <section class="signupnext">
     <div class="body">
-      <h1 class="heading">Please complete your registration: {{credentials.email}}</h1>
+      <h1 class="heading">Please complete your registration {{credentials.email}}</h1>
       <p class="subheading">In a consequat mi. Etiam sit amet diam in diam ullamcorper
         consequat. Maecenas pellentesque mauris augue, in scelerisque lectus imperdiet et.
         Nullam ultricies, eros quis maximus.
       </p>
       <form @submit.prevent="onSubmit()">
         <div class="form-group">
-          <label class="control-label">Username</label>
+          <label class="control-label">Email Address</label>
           <input type="text"
                  id="username"
-                 placeholder="Enter username"
+                 placeholder="example@email.com"
                  class="form-control"
                  v-model="credentials.username">
         </div>
@@ -19,7 +19,7 @@
           <label class="control-label">Password</label>
           <input type="text"
                  id="password"
-                 placeholder="Enter password"
+                 placeholder="Enter your password"
                  class="form-control"
                  v-model="credentials.password">
         </div>
@@ -27,7 +27,7 @@
           <label class="control-label">Repeat Password</label>
           <input type="text"
                  id="passwordConfirm"
-                 placeholder="Confirm password"
+                 placeholder="Retype your password"
                  class="form-control"
                  v-model="credentials.confirmPassword">
         </div>
@@ -35,6 +35,7 @@
           <div class="checkbox long-text">
             <label>
               <input class="checkbox" type="checkbox"/>
+              <span></span>
               I acknowledge that I have read and accept the Terms of Use Agreement
               and consent to the Privacy Policy and Video Privacy Policy.</label>
           </div>
@@ -43,6 +44,7 @@
           <div class="checkbox">
             <label>
               <input class="checkbox2" type="checkbox"/>
+              <span></span>
               Subscribe to our Newsletter</label>
           </div>
         </div>
@@ -54,12 +56,12 @@
   </section>
 </template>
 <script>
-import moment from 'moment';
-import { mapActions, mapGetters, mapMutations } from 'vuex';
-import * as types from '../store/types';
+import moment from "moment";
+import { mapActions, mapGetters, mapMutations } from "vuex";
+import * as types from "../store/types";
 
 export default {
-  name: 'signupnext',
+  name: "signupnext",
   data() {
     return {};
   },
@@ -67,26 +69,30 @@ export default {
     ...mapGetters({
       status: types.ACCOUNTS_LOGIN_STATUS,
       token: types.ACCOUNTS_TOKEN,
-      credentials: types.ACCOUNTS_USER_CREDENTIALS,
-    }),
+      credentials: types.ACCOUNTS_USER_CREDENTIALS
+    })
   },
   methods: {
     ...mapActions({
-      signup: types.ACCOUNTS_SIGNUP,
+      signup: types.ACCOUNTS_SIGNUP
     }),
     ...mapMutations({
-      status: types.ACCOUNTS_LOGIN_STATUS,
+      status: types.ACCOUNTS_LOGIN_STATUS
     }),
     onSubmit() {
       if (this.credentials.password !== this.credentials.confirmPassword) {
-        this.status('Passwords do not match');
+        this.status("Passwords do not match");
         return false;
       }
 
       delete this.credentials.confirmPassword;
 
-      this.credentials.date_created = moment(new Date()).format('YYYY-MM-DD hh:mm:ss');
-      this.credentials.date_of_birth = moment('1970-01-01').format('YYYY-MM-DD');
+      this.credentials.date_created = moment(new Date()).format(
+        "YYYY-MM-DD hh:mm:ss"
+      );
+      this.credentials.date_of_birth = moment("1970-01-01").format(
+        "YYYY-MM-DD"
+      );
 
       return this.signup(this.credentials).then(() => {
         this.$router.push('/');
@@ -97,8 +103,23 @@ export default {
 </script>
 <style lang="scss" scoped>
 .signupnext {
-  .subheading {
-    line-height: 1.8rem;
+  .body {
+    max-width: 50rem !important;
+    .heading {
+      text-align: center;
+      font-size: 4rem;
+      margin-bottom: 4rem;
+    }
+    .subheading {
+      text-align: center;
+      margin-bottom: 4rem;
+    }
+    .long-text {
+      margin-top: 4rem;
+    }
+    .cta {
+      margin-top: 4rem;
+    }
   }
 }
 </style>
